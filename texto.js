6886cbeb7e5f15f590432b1cacb5d1fb3f165212
JCardilho <115733809+JCardilho@users.noreply.github.com>
2023-11-22 22:47:07 -0300

    Add files via upload

---
+++ texto.js
@@ -0,0 +1,17 @@
+const modifica_1paragrafo = document.querySelector(".paragrafo");
+const todosParagrafo= document.querySelectorAll(".paragrafo");
+
+modifica_1paragrafo.textContent = "Este paragrafo foi Modificado ";
+todosParagrafo.forEach(meuDiv => { 
+    meuDiv.textContent = "Todos os paragrafo foram modificados as querySelector"
+});
+
+document.getElementById("novoDiv").textContent = "Funcionou!";
+
+
+// const todosParagrafo = [
+//     <p class="paragrafo">Este texto é o original</p>,
+//     <p class="paragrafo">Este texto é o original</p>,
+//     <p class="paragrafo">Este texto é o original</p>,
+//     <p class="paragrafo">Este texto é o original</p>
+// ]
